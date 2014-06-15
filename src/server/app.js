fab3f91d4891c28720644108e1f86ff69d8cf98e
Shalom Getz <shalom.getz@gmail.com>
2014-06-14 21:48:05 -0400

    api server middleware added

--- src/server/app.js
+++ src/server/app.js
@@ -2,15 +2,23 @@ require('coffee-script/register');
 var express = require('express');
 var app = express();
 
+//TODO: move to a separate setup file
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var methodOverride = require('method-override');
+var logger = require('connect-logger');
+var cors = require('connect-cors');
+
+app.use(logger());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({ secret: 'cool beans' }));
+app.use(methodOverride());
+app.use(cors({origins:['*']}));
+
+//import and mount all the routes
 require('./routes')(app);
-
-//TODO move the cores config to different file
-app.all("*", function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 
 module.exports = app;
--- src/server/routes/test.coffee
+++ src/server/routes/test.coffee
@@ -1,3 +1,3 @@
 module.exports = (app) ->
-	app.get '/test', (req, res) ->
-		res.send {test: 'Hello world ! It Works!'}
+  app.get '/test', (req, res) ->
+    res.send {test: 'Hello world ! It Works!'}
